3caab0a971f73dba6c4e04142edad92628a3badf
anooptejt <30777716+anooptejt@users.noreply.github.com>
2019-01-22 12:09:09 +0530

    Updated by Anoop
    Modified the script as per the Requirement..

--- TriggerCanary.sh
+++ TriggerCanary.sh
@@ -3,7 +3,7 @@
 usage="$(basename "$0")
 [-help]  -- Example to show how to pass relevant arguments.
 USAGE:
-$(basename "$0") servername='12.299.222.155' username='johndoe' metrictemplate='apptemplate' logtemplate='elastictemplate' lifetimeHours=0.5 canaryAnalysisIntervalMins='30' baseline=1e8ecb994cbba canary=1e8ecb994cbba baselineStartMs=1528139701000 canaryResultScore='60' minimumCanaryScore=50"
+$(basename "$0") servername=12.299.222.155 username=johndoe metrictemplate=metric-template-name logtemplate=log-template-name lifetimeHours=0.5 canaryAnalysisIntervalMins=30 baseline=1e8ecb994cbba canary=1e8ecb994cbba baselineStartTimeMs=1528139701000 canaryStartTimeMs=1528139701000 canaryResultScore=90 minimumCanaryScore=70"
 
 while getopts ':hhelp:' option; do
   case "$option" in
@@ -12,8 +12,8 @@ while getopts ':hhelp:' option; do
        exit 1 ;;
   esac
 done
-if  [ "$#" -lt 11 ] || [ $# -eq 0 ]; then
-    echo "Should pass sufficent arguments. For help pass -help"
+if  [ $# -eq 0 ]; then
+    echo "Please pass the mandatory arguments. To know about the required arguments type option -help"
     exit 1
 fi
 for ARGUMENT in "$@"
@@ -21,80 +21,159 @@ do
 
     KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
     VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)
-
+    echo  $KEY $VALUE
     case "$KEY" in
             servername)
                if [ -z "${VALUE}" ];then
-                 echo "servername not found!"
+                 echo "Value for mandatory parameter 'servername' is not specified!"
                fi
             servername=${VALUE} ;;
             canaryAnalysisIntervalMins)
                if [ -z "${VALUE}" ];then
-                 echo "canaryAnalysisIntervalMins not found!"
+                 echo "Value for mandatory parameter 'canaryAnalysisIntervalMins' is not specified!"
                fi
             canaryAnalysisIntervalMins=${VALUE} ;;
             metrictemplate)
                if [ -z "${VALUE}" ];then
-                 echo "metrictemplate not found!"
+                 echo "Value for mandatory parameter 'metrictemplate' is not specified!"
                fi
             metrictemplate=${VALUE};;
             logtemplate)
                 if [ -z "${VALUE}" ];then
-                 echo "logtemplate not found!"
+                 echo "Value for mandatory paramter 'logtemplate' is not specified!"
                fi
             logtemplate=${VALUE};;
             minimumCanaryScore)
                if [ -z "${VALUE}" ];then
-                  echo "minimumCanaryScore not found!"
+                  echo "Value for optional parameter 'minimumCanaryScore' is not specified in the arguments, assigning the default value to 70!"
+		  value=70
                fi
             minimumCanaryScore=${VALUE};;
             canaryResultScore)
                if [ -z "${VALUE}" ];then
-                 echo "canaryResultScore not found!"
+                 echo "Value for optional parameter 'canaryResultScore' is not specified in the arguments, assigning the default value to 90!"
+		 value=90
                fi
             canaryResultScore=${VALUE};;
             lifetimeHours)
                if [ -z "${VALUE}" ];then
-                 echo "lifetimeHours not found!"
+                 echo "Value for mandatory parameter 'lifetimeHours' is not specified!"
                fi
             lifetimeHours=${VALUE};;
             username)
                if [ -z "${VALUE}" ];then
-                 echo "username not found!"
+                 echo "Value for mandatory parameter 'username' is not specified!"
                fi
             username=${VALUE};;
             baseline)
                if [ -z "${VALUE}" ];then
-                 echo "baseline not found!"
+                 echo "Value for mandatory parameter 'baseline' is not specified!"
                fi
             baseline=${VALUE};;
-            baselineStartMs)
+            baselineStartTimeMs)
               if [ -z "${VALUE}" ];then
-                 echo "baselineStartMs not found!"
+                 echo "Value for mandatory parameter 'baselineStartTimeMs' is not specified!"
                fi
-            baselineStartMs=${VALUE};;
+            baselineStartTimeMs=${VALUE};;
             canary)
                if [ -z "${VALUE}" ];then
-                 echo "canary not found!"
+                 echo "Value for mandatory parameter 'canary' is not specified!"
                fi
             canary=${VALUE};;
+ 	    canaryStartTimeMs)
+              if [ -z "${VALUE}" ];then
+                 echo "Value for mandatory parameter 'canaryStartTimeMs' is not specified!"
+               fi
+            canaryStartTimeMs=${VALUE};;
             *)
     esac
 
 done
 
-# checking for optional parameters if not present (baselineStartMs)
-
-if [ -z $baselineStartMs ]; then
-  baselineStartMs='null'
+# Checking for mandatory parameters, if not present
+counter=0
+MESSAGE="Please pass the following required parameters as arguments : "
+if [ -z $servername ]; then
+   counter=$((counter+1))
+   if  [ $counter -eq 1 ]; then
+	echo $MESSAGE
+   fi
+   echo $counter ". 'servername'"
+fi
+if [ -z $username ]; then
+   counter=$((counter+1))
+   if  [ $counter -eq 1 ]; then
+	echo $MESSAGE
+   fi
+   echo $counter ". 'username'"
+fi
+if [ -z $lifetimeHours ]; then
+   counter=$((counter+1))
+   if  [ $counter -eq 1 ]; then
+	echo $MESSAGE
+   fi
+   echo $counter ". 'lifetimeHours'"
+fi
+if [ -z $canaryAnalysisIntervalMins ]; then
+   counter=$((counter+1))
+   if  [ $counter -eq 1 ]; then
+	echo $MESSAGE
+   fi
+   echo $counter ". 'canaryAnalysisIntervalMins'"
+fi
+if [ -z $baseline ]; then
+   counter=$((counter+1))
+   if  [ $counter -eq 1 ]; then
+	echo $MESSAGE
+   fi
+   echo $counter ". 'baseline'"
+fi
+if [ -z $canary ]; then
+   counter=$((counter+1))
+   if  [ $counter -eq 1 ]; then
+	echo $MESSAGE
+   fi
+   echo $counter ". 'canary'"
+fi
+if [ -z $baselineStartTimeMs ]; then
+   counter=$((counter+1))
+   if  [ $counter -eq 1 ]; then
+	echo $MESSAGE
+   fi
+   echo $counter ". 'baselineStartTimeMs'"
+fi
+if [ -z $canaryStartTimeMs ]; then
+   counter=$((counter+1))
+   if  [ $counter -eq 1 ]; then
+	echo $MESSAGE
+   fi
+   echo $counter ". 'canaryStartTimeMs'"
+fi
+if  [ $counter -gt 0 ]; then
+       exit 1
 fi
 
-if [ -z $logtemplate ]; then
-  logtemplate='null'
+if [ -z $metrictemplate ] && [ -z $logtemplate ]; then
+  echo "Aleast one of the parameters 'metrictemplate' or 'logtemplate' is required"
+  exit 1
+fi
+
+# Setting optional parameters, if not specified by user
+if [ -z $minimumCanaryScore ];then
+   echo "Optional parameter 'minimumCanaryScore' is not specified in the arguments, assigning the default value to 70!"
+   minimumCanaryScore=70
+fi
+if [ -z $canaryResultScore ];then
+   echo "Value for parameter 'canaryResultScore' is not specified in the arguments, assigning the default value to 90!"
+   canaryResultScore=90
 fi
 
 url="http://$servername:8090/registerCanary"
-jsondata="{\"application\":\"prodk8\", \"canaryConfig\":{ \"canaryAnalysisConfig\":{ \"beginCanaryAnalysisAfterMins\": \"0\",\"canaryAnalysisIntervalMins\" : \"$canaryAnalysisIntervalMins\",  \"lookbackMins\" : 0, \"name\" : \"metric-template:$metrictemplate;log-template:$logtemplate\", \"notificationHours\" : [ ], \"useLookback\" : false }, \"canaryHealthCheckHandler\" : {\"@class\" : \"com.netflix.spinnaker.mine.CanaryResultHealthCheckHandler\", \"minimumCanaryResultScore\" : \"$minimumCanaryScore\"}, \"canarySuccessCriteria\" : { \"canaryResultScore\" : \"$canaryResultScore\" },\"combinedCanaryResultStrategy\" : \"AGGREGATE\", \"lifetimeHours\" : \"$lifetimeHours\", \"name\" : \"$username\", \"application\" : \"prodk8\"}, \"canaryDeployments\" : [ { \"@class\" : \".CanaryTaskDeployment\", \"accountName\" : \"my-k8s-account\", \"baseline\" : \"$baseline\", \"baselineStartMs\": "$baselineStartMs", \"canary\" : \"$canary\", \"type\" : \"cluster\" } ], \"watchers\" : [ ]}"
+jsondata="{\"application\":\"prodk8\", \"canaryConfig\":{ \"canaryAnalysisConfig\":{ \"beginCanaryAnalysisAfterMins\": \"0\",\"canaryAnalysisIntervalMins\" : \"$canaryAnalysisIntervalMins\",  \"lookbackMins\" : 0, \"name\" : \"metric-template:$metrictemplate;log-template:$logtemplate\", \"notificationHours\" : [ ], \"useLookback\" : false }, \"canaryHealthCheckHandler\" : {\"@class\" : \"com.netflix.spinnaker.mine.CanaryResultHealthCheckHandler\", \"minimumCanaryResultScore\" : \"$minimumCanaryScore\"}, \"canarySuccessCriteria\" : { \"canaryResultScore\" : \"$canaryResultScore\" },\"combinedCanaryResultStrategy\" : \"AGGREGATE\", \"lifetimeHours\" : \"$lifetimeHours\", \"name\" : \"$username\", \"application\" : \"prodk8\"}, \"canaryDeployments\" : [ { \"@class\" : \".CanaryTaskDeployment\", \"accountName\" : \"my-k8s-account\", \"baseline\" : \"$baseline\", \"baselineStartTimeMs\": "$baselineStartTimeMs", \"canary\" : \"$canary\", \"canaryStartTimeMs\": "$canaryStartTimeMs", \"type\" : \"cluster\" } ], \"watchers\" : [ ]}"
 
+echo "Request body : "
+echo $jsondata
 response=$(curl -H  "Content-Type:application/json"  -X POST -d "$jsondata" "$url")
-echo $response
+echo "Canary Id : $response"
+echo "Report URL : http://$servername:8161/opsmx-analysis/public/canaryAnalysis.html#/analysis/$response"
+
